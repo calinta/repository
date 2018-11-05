120829b3b0928affd40cb0d6e23027b142716dce
calen <calenjay254@gmail.com>
2018-11-05 12:49:33 +0300

    The work

---
+++ tutorials/application/views/users/register.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>registration</title>
+</head>
+<body>
+
+  <hr>
+       <label for="Name "><b>Name</b></label>
+      <input type="text" placeholder="Enter Name" name="name" required><br>
+      <br/>
+
+      <label for="email"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" required><br>
+       <br/>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required><br>
+         <br/>
+      
+      <label for="psw-repeat"><b>Repeat Password</b></label>
+      <input type="password" placeholder="Repeat Password" name="psw-repeat" required><br>
+        <br/>
+
+        <button type="submit">sign up</button>
+
+
+      <label>
+        <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+      </label>
+    </hr>
+
+</body>
+</html>
